c327a5d3b813dbe2540c6db9f13241ebe57939d9
Calvin Brown <calvin.brown@banno.com>
2019-04-05 14:04:09 -0500

    Implementing main gameplay for rock, paper scissors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
+use std::io;
+use std::fmt::Error;
+mod weapon;
+use weapon::*;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("Rock, Paper, or Scissors?");
+
+        let mut choice = String::new();
+
+        io::stdin().read_line(&mut choice).unwrap();
+
+        let weapon: Weapon = get_user_weapon(choice.to_lowercase().trim());
+        let bot_weapon: Weapon = get_bot_weapon();
+
+        play_round(&weapon, &bot_weapon);
+    }
+
+
+}
+
+fn play_round(user_weapon: &Weapon, bot_weapon: &Weapon) {
+    fn handle_rock(bot_weapon: &Weapon) -> bool {
+        match bot_weapon {
+            Weapon::ROCK => true,
+            Weapon::PAPER => false,
+            Weapon::SCISSORS => true,
+        }
+    }
+    fn handle_paper(bot_weapon: &Weapon) -> bool {
+        match bot_weapon {
+            Weapon::ROCK => true,
+            Weapon::PAPER => true,
+            Weapon::SCISSORS => false,
+        }
+    }
+    fn handle_scissors(bot_weapon: &Weapon) -> bool {
+        match bot_weapon {
+            Weapon::ROCK => false,
+            Weapon::PAPER => true,
+            Weapon::SCISSORS => true,
+        }
+    }
+    let result = match user_weapon {
+        Weapon::ROCK => handle_rock(bot_weapon),
+        Weapon::PAPER => handle_paper(bot_weapon),
+        Weapon::SCISSORS => handle_scissors(bot_weapon),
+    };
+
+    match result {
+        true => println!("You win! You chose: {:?} and the bot chose {:?}", user_weapon, bot_weapon),
+        false => println!("You lose! You chose: {:?} and the bot chose {:?}", user_weapon, bot_weapon),
+    }
+
+
 }
---
+++ src/weapon.rs
@@ -0,0 +1,27 @@
+use rand::Rng;
+
+#[derive(Debug)]
+pub enum Weapon {
+    ROCK,
+    PAPER,
+    SCISSORS
+}
+
+pub fn get_user_weapon(player_choice: &str) -> Weapon {
+    match player_choice {
+        "rock" => Weapon::ROCK,
+        "paper" => Weapon::PAPER,
+        "scissors" => Weapon::SCISSORS,
+        _ => panic!("Error"),
+    }
+}
+
+pub fn get_bot_weapon() -> Weapon {
+    let secret_number = rand::thread_rng().gen_range(1, 4);
+    match secret_number {
+        1 => Weapon::ROCK,
+        2 => Weapon::PAPER,
+        3 => Weapon::SCISSORS,
+        _ => panic!("Should never get here!"),
+    }
+}
